62384bbd76269398207736345d6523024e50c714
Nez Mustafa <nezmustafa123@gmail.com>
2021-02-28 21:09:29 +0000

    selecting muliple elements with same class using queryselectorall

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//select elements chuck them in variables
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+//have to use querySelectorAll to get node list similar to an array
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+for(let i = 0; i< btnsOpenModal.length; i++) {
+    console.log(btnsOpenModal[i].textConent);
+    
+}
